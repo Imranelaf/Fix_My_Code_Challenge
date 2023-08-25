76a4fb9c693ca85339b60bd82f65a4f1c2837cc6
Imrane ALI LAFKIH <imrane.laf@gmail.com>
2023-08-25 13:17:19 -0700

    Print square

--- 1-print_square.js
+++ 1-print_square.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 /*
     Print a square with the character #
-    
-    The size of the square must be the first argument 
-    of the program.
 */
 
 
@@ -11,18 +8,13 @@ if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.exit(1)
 }
 
-size = parseInt(process.argv[2], 10); // Use base 10 for decimal numbers
+size = parseInt(process.argv[2], 16)
 
-if (isNaN(size)) {
-    process.stderr.write("Invalid size argument. Please provide a valid number.\n");
-    process.exit(1);
-}
-
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
